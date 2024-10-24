9913b1ac179d83e71550042533d5c338732e3a22
pawelszwarno <pawelszwarno@student.agh.edu.pl>
2024-10-24 09:12:01 +0200

    Create basic project structure

---
+++ inc/cps_collision_detection.h
@@ -0,0 +1,14 @@
+#ifndef __CPS_COLLISION_DETECTION__
+#define __CPS_COLLISION_DETECTION__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __CPS_COLLISION_DETECTION__
---
+++ src/cps_collision_detection.c
@@ -0,0 +1,9 @@
+#include "cps_collision_detection.h"
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("Hello world!\n");
+    return 0;
+}
